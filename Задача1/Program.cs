8bad30e803f6c3196a0e738c3cb268fd06bec5a4
Katerina <m-a-c-u-k-a@mail.ru>
2022-09-16 16:32:38 +0700

    Возведение в степень

---
+++ Задача1/Program.cs
@@ -0,0 +1,17 @@
+﻿int Exponentiation(int numberA, int numberB)
+{
+ int result = 1;
+ for(int i=1; i <= numberB; i++)
+    {
+    result = result * numberA;
+    }
+    return result;
+}
+
+ Console.Write("Введите число A: ");
+ int numberA = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Введите число B: ");
+ int numberB = Convert.ToInt32(Console.ReadLine());
+
+ int exponentiation = Exponentiation(numberA, numberB);
+ Console.WriteLine("Ответ: " + exponentiation);
